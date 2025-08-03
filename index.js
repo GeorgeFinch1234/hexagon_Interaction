ff460fe40b29a86a335d8bb7eccbe51bedd53145
George Finch <georgefinch12@gmail.com>
2025-08-03 13:58:25 +0100

    code tidying up.
    removed uneeded code.
    improved indenation.

--- index.js
+++ index.js
@@ -6,27 +6,27 @@ const gridSize = 20;
 let clickXLocation = 0;
 let clickYLocation = 0;
 
- ctx.fillStyle = "rgb(0,0,0)"
-        ctx.fillRect(0, 0, window.innerWidth, window.innerHeight)
+ctx.fillStyle = "rgb(0,0,0)"
+ctx.fillRect(0, 0, window.innerWidth, window.innerHeight)
 
 /*
 point is so class know how many time it has moves, so each click send say a line of and it does 3 movments
 */
 
-class line{
-    constructor(initalX,initalY){
+class line {
+    constructor(initalX, initalY) {
         this.X = initalX
         this.Y = initalY
         //bascilly a base case for recursion
         this.moveLeft = 35
     }
-    drawHexagons(){
-        draw(this.X,this.Y,this)
-        this.moveLeft = this.moveLeft -1;
+    drawHexagons() {
+        draw(this.X, this.Y, this)
+        this.moveLeft = this.moveLeft - 1;
         //so draws 3 lines
-        if(!(this.moveLeft <0)){
-            setTimeout(()=>{this.drawHexagons()},15)
-        } 
+        if (!(this.moveLeft < 0)) {
+            setTimeout(() => { this.drawHexagons() }, 15)
+        }
     }
 }
 
@@ -38,14 +38,14 @@ bascially sets where clint clicked then draws.
 addEventListener("click", (e) => {
     clickXLocation = e.clientX
     clickYLocation = e.clientY
-     new line(clickXLocation,clickYLocation).drawHexagons()
-    
+    new line(clickXLocation, clickYLocation).drawHexagons()
+
 })
 addEventListener("resize", () => {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
- ctx.fillStyle = "rgb(0,0,0)"
-        ctx.fillRect(0, 0, window.innerWidth, window.innerHeight)
+    ctx.fillStyle = "rgb(0,0,0)"
+    ctx.fillRect(0, 0, window.innerWidth, window.innerHeight)
 
 })
 /*
@@ -69,7 +69,7 @@ function chunkX(x) {
     
     
     */
-   
+
     return (Math.round(x / gridSize) % 6);
 
 }
@@ -87,7 +87,7 @@ function chunkY(y) {
 /*
 x and y are the start location, line is an object so can uodate it for next run
 */
-function draw(X, Y,Line) {
+function draw(X, Y, Line) {
     /*
 Math.ceil - >rounds up so whole number 
 Math.ceil(clickXLocation/gridSize) - >put it on the grid, 
@@ -135,314 +135,314 @@ off set need to be by 3 places so if gridY = 1 then 0 = 3,1=4,2=5 and so on
 why 
 as each row is just \__/-- to keep life sympole
         */
-        
-       if(gridY==0) {
-        
-        /*
+
+        if (gridY == 0) {
+
+            /*
+          \__/--\__
+            0 can go, lef, rightup right down
+              */
+
+            if (pickingThree == 1) {
+                //left
+                ctx.lineTo(postionX - gridSize, postionY);
+                Line.X = postionX - gridSize
+
+            } else if (pickingThree == 2) {
+                //right up
+                ctx.lineTo(postionX + gridSize, postionY - gridSize);
+                Line.X = postionX + gridSize
+                Line.Y = postionY - gridSize
+            } else {
+                //rigth down
+                ctx.lineTo(postionX + gridSize, postionY + gridSize);
+                Line.X = postionX + gridSize
+                Line.Y = postionY + gridSize
+            }
+
+        } else {
+            ctx.moveTo(postionX, postionY + gridSize)
+            /*
       \__/--\__
-        0 can go, lef, rightup right down
+     3 can go left, rigth up, or right down
           */
 
-        if (pickingThree == 1) {
-            //left
-            ctx.lineTo(postionX - gridSize, postionY);
-            Line.X = postionX - gridSize
-           
-        } else if (pickingThree == 2) {
-            //right up
-            ctx.lineTo(postionX + gridSize, postionY - gridSize);
-            Line.X = postionX + gridSize
-            Line.Y = postionY - gridSize
-        } else {
-            //rigth down
-            ctx.lineTo(postionX + gridSize, postionY + gridSize);
-            Line.X = postionX + gridSize
-            Line.Y = postionY + gridSize
-        }
+            if (pickingThree == 1) {
+                //left
+                ctx.lineTo(postionX - gridSize, postionY + gridSize);
+                Line.X = postionX - gridSize
+                Line.Y = postionY + gridSize
+            } else if (pickingThree == 2) {
+                //right up
+                ctx.lineTo(postionX + gridSize, postionY);
+                Line.X = postionX + gridSize
 
-    }else{ 
-        ctx.moveTo(postionX, postionY + gridSize)
-        /*
-  \__/--\__
- 3 can go left, rigth up, or right down
-      */
-
-        if (pickingThree == 1) {
-            //left
-            ctx.lineTo(postionX - gridSize, postionY + gridSize);
-            Line.X = postionX - gridSize
-            Line.Y = postionY + gridSize
-        } else if (pickingThree == 2) {
-            //right up
-            ctx.lineTo(postionX + gridSize, postionY);
-            Line.X = postionX + gridSize
-            
-        } else {
-            //right down
-            ctx.lineTo(postionX + gridSize, postionY + gridSize + gridSize);
-            Line.X = postionX + gridSize
-            Line.Y = postionY + gridSize+gridSize
-        }
+            } else {
+                //right down
+                ctx.lineTo(postionX + gridSize, postionY + gridSize + gridSize);
+                Line.X = postionX + gridSize
+                Line.Y = postionY + gridSize + gridSize
+            }
 
 
         }
 
-    
+
     } else if (gridX == 1) {
-       
-       if(gridY==0) {
-      
-        /*
-   \__/--\__
-    1 can go right, left up, left down 
-     
-    ideas is intial click get top line as starting point
-    so as want to start lower have to ajust for that
-    */
-        ctx.moveTo(postionX, postionY + gridSize)
-        if (pickingThree == 1) {
-            //right
-            ctx.lineTo(postionX + gridSize, postionY + gridSize);
-            Line.X = postionX + gridSize
-            Line.Y = postionY + gridSize
-        } else if (pickingThree == 2) {
-            //left up
-            ctx.lineTo(postionX - gridSize, postionY);
-            Line.X = postionX - gridSize
-            
+
+        if (gridY == 0) {
+
+            /*
+       \__/--\__
+        1 can go right, left up, left down 
+         
+        ideas is intial click get top line as starting point
+        so as want to start lower have to ajust for that
+        */
+            ctx.moveTo(postionX, postionY + gridSize)
+            if (pickingThree == 1) {
+                //right
+                ctx.lineTo(postionX + gridSize, postionY + gridSize);
+                Line.X = postionX + gridSize
+                Line.Y = postionY + gridSize
+            } else if (pickingThree == 2) {
+                //left up
+                ctx.lineTo(postionX - gridSize, postionY);
+                Line.X = postionX - gridSize
+
+            } else {
+                //left down
+                ctx.lineTo(postionX - gridSize, postionY + gridSize + gridSize);
+                Line.X = postionX - gridSize
+                Line.Y = postionY - gridSize + gridSize
+            }
+
         } else {
-            //left down
-            ctx.lineTo(postionX - gridSize, postionY + gridSize + gridSize);
-            Line.X = postionX - gridSize
-            Line.Y = postionY - gridSize + gridSize
+
+            /*
+       \__/--\__
+       4 can go righ, left down, left up
+           */
+
+            if (pickingThree == 1) {
+                //right
+                ctx.lineTo(postionX + gridSize, postionY);
+                Line.X = postionX + gridSize
+
+            } else if (pickingThree == 2) {
+                //left down
+                ctx.lineTo(postionX - gridSize, postionY + gridSize);
+                Line.X = postionX - gridSize
+                Line.Y = postionY + gridSize
+            } else {
+                //left up
+                ctx.lineTo(postionX - gridSize, postionY - gridSize);
+                Line.X = postionX - gridSize
+                Line.Y = postionY - gridSize
+            }
+
+
         }
-
-    }else{
-   
-        /*
-   \__/--\__
-   4 can go righ, left down, left up
-       */
-
-        if (pickingThree == 1) {
-            //right
-            ctx.lineTo(postionX + gridSize, postionY);
-            Line.X = postionX + gridSize
-            
-        } else if (pickingThree == 2) {
-            //left down
-            ctx.lineTo(postionX - gridSize, postionY + gridSize);
-            Line.X = postionX - gridSize
-            Line.Y = postionY + gridSize
-        } else {
-            //left up
-            ctx.lineTo(postionX - gridSize, postionY - gridSize);
-            Line.X = postionX - gridSize
-            Line.Y = postionY - gridSize
-        }
-
-
-    }
 
 
 
 
     } else if (gridX == 2) {
-        if(gridY==0) {
-           
-        /*
-\__/--\__
-2 can go left or right 
-*/
-        ctx.moveTo(postionX, postionY + gridSize)
-        if (pickingTwo == 1) {
-            //left
-            ctx.lineTo(postionX - gridSize, postionY + gridSize);
-            Line.X = postionX - gridSize
-            Line.Y = postionY + gridSize
-        } else {
-            //right
-            ctx.lineTo(postionX + gridSize, postionY + gridSize);
-            Line.X = postionX + gridSize
-            Line.Y = postionY + gridSize
-        }
-    }else {console.log("x=2 y=1")
-        /*
+        if (gridY == 0) {
+
+            /*
     \__/--\__
-    5 can go rigth or left
-        */
-
-        if (pickingTwo == 1) {
-            //right
-            ctx.lineTo(postionX + gridSize, postionY);
-            Line.X = postionX + gridSize
-            
+    2 can go left or right 
+    */
+            ctx.moveTo(postionX, postionY + gridSize)
+            if (pickingTwo == 1) {
+                //left
+                ctx.lineTo(postionX - gridSize, postionY + gridSize);
+                Line.X = postionX - gridSize
+                Line.Y = postionY + gridSize
+            } else {
+                //right
+                ctx.lineTo(postionX + gridSize, postionY + gridSize);
+                Line.X = postionX + gridSize
+                Line.Y = postionY + gridSize
+            }
         } else {
-            //left
-            ctx.lineTo(postionX - gridSize, postionY);
-            Line.X = postionX - gridSize
-           
+            /*
+        \__/--\__
+        5 can go rigth or left
+            */
+
+            if (pickingTwo == 1) {
+                //right
+                ctx.lineTo(postionX + gridSize, postionY);
+                Line.X = postionX + gridSize
+
+            } else {
+                //left
+                ctx.lineTo(postionX - gridSize, postionY);
+                Line.X = postionX - gridSize
+
+            }
         }
-    }
     } else if (gridX == 3) {
-if(gridY==0) {
-    console.log("x=3 y=0")
-        ctx.moveTo(postionX, postionY + gridSize)
-        /*
-  \__/--\__
- 3 can go left, rigth up, or right down
-      */
+        if (gridY == 0) {
 
-        if (pickingThree == 1) {
-            //left
-            ctx.lineTo(postionX - gridSize, postionY + gridSize);
-            Line.X = postionX - gridSize
-            Line.Y = postionY + gridSize
-        } else if (pickingThree == 2) {
-            //right up
-            ctx.lineTo(postionX + gridSize, postionY);
-            Line.X = postionX + gridSize
-            
-        } else {
-            //right down
-            ctx.lineTo(postionX + gridSize, postionY + gridSize + gridSize);
-            Line.X = postionX + gridSize
-            Line.Y = postionY + gridSize + gridSize
-        }
-
-
-
-
-
-
-    }else{
-     console.log("x=3 y=1")
-        /*
+            ctx.moveTo(postionX, postionY + gridSize)
+            /*
       \__/--\__
-        0 can go, lef, rightup right down
+     3 can go left, rigth up, or right down
           */
 
-        if (pickingThree == 1) {
-            //left
-            ctx.lineTo(postionX - gridSize, postionY);
-            Line.X = postionX - gridSize
-            
-        } else if (pickingThree == 2) {
-            //right up
-            ctx.lineTo(postionX + gridSize, postionY - gridSize);
-            Line.X = postionX + gridSize
-            Line.Y = postionY - gridSize
+            if (pickingThree == 1) {
+                //left
+                ctx.lineTo(postionX - gridSize, postionY + gridSize);
+                Line.X = postionX - gridSize
+                Line.Y = postionY + gridSize
+            } else if (pickingThree == 2) {
+                //right up
+                ctx.lineTo(postionX + gridSize, postionY);
+                Line.X = postionX + gridSize
+
+            } else {
+                //right down
+                ctx.lineTo(postionX + gridSize, postionY + gridSize + gridSize);
+                Line.X = postionX + gridSize
+                Line.Y = postionY + gridSize + gridSize
+            }
+
+
+
+
+
+
         } else {
-            //rigth down
-            ctx.lineTo(postionX + gridSize, postionY + gridSize);
-            Line.X = postionX + gridSize
-            Line.Y = postionY + gridSize
+
+            /*
+          \__/--\__
+            0 can go, lef, rightup right down
+              */
+
+            if (pickingThree == 1) {
+                //left
+                ctx.lineTo(postionX - gridSize, postionY);
+                Line.X = postionX - gridSize
+
+            } else if (pickingThree == 2) {
+                //right up
+                ctx.lineTo(postionX + gridSize, postionY - gridSize);
+                Line.X = postionX + gridSize
+                Line.Y = postionY - gridSize
+            } else {
+                //rigth down
+                ctx.lineTo(postionX + gridSize, postionY + gridSize);
+                Line.X = postionX + gridSize
+                Line.Y = postionY + gridSize
+            }
         }
-    }
-    
+
 
     }
 
-else if(gridX==4){
-    if(gridY==0){
- console.log("x=4 y=0")
-        /*
-   \__/--\__
-   4 can go righ, left down, left up
-       */
+    else if (gridX == 4) {
+        if (gridY == 0) {
 
-        if (pickingThree == 1) {
-            //right
-            ctx.lineTo(postionX + gridSize, postionY);
-            Line.X = postionX + gridSize
-            
-        } else if (pickingThree == 2) {
-            //left down
-            ctx.lineTo(postionX - gridSize, postionY + gridSize);
-            Line.X = postionX - gridSize
-            Line.Y = postionY + gridSize
+            /*
+       \__/--\__
+       4 can go righ, left down, left up
+           */
+
+            if (pickingThree == 1) {
+                //right
+                ctx.lineTo(postionX + gridSize, postionY);
+                Line.X = postionX + gridSize
+
+            } else if (pickingThree == 2) {
+                //left down
+                ctx.lineTo(postionX - gridSize, postionY + gridSize);
+                Line.X = postionX - gridSize
+                Line.Y = postionY + gridSize
+            } else {
+                //left up
+                ctx.lineTo(postionX - gridSize, postionY - gridSize);
+                Line.X = postionX - gridSize
+                Line.Y = postionY - gridSize
+            }
+
         } else {
-            //left up
-            ctx.lineTo(postionX - gridSize, postionY - gridSize);
-            Line.X = postionX - gridSize
-            Line.Y = postionY - gridSize
-        }
 
-    }else{
-console.log("x=4 y=1")
-        /*
-   \__/--\__
-    1 can go right, left up, left down 
-     
-    ideas is intial click get top line as starting point
-    so as want to start lower have to ajust for that
-    */
-        ctx.moveTo(postionX, postionY + gridSize)
-        if (pickingThree == 1) {
-            //right
-            ctx.lineTo(postionX + gridSize, postionY + gridSize);
-            Line.X = postionX + gridSize
-            Line.Y = postionY + gridSize
-        } else if (pickingThree == 2) {
-            //left up
-            ctx.lineTo(postionX - gridSize, postionY);
-            Line.X = postionX - gridSize
-            
-        } else {
-            //left down
-            ctx.lineTo(postionX - gridSize, postionY + gridSize + gridSize);
-             Line.X = postionX - gridSize
-            Line.Y = postionY + gridSize + gridSize
-            
-        }
-    }
-}
-
-
-     else if (gridX == 5) {
-if(gridY==0) {
-console.log("x=5 y=0")
-        /*
-    \__/--\__
-    5 can go rigth or left
+            /*
+       \__/--\__
+        1 can go right, left up, left down 
+         
+        ideas is intial click get top line as starting point
+        so as want to start lower have to ajust for that
         */
+            ctx.moveTo(postionX, postionY + gridSize)
+            if (pickingThree == 1) {
+                //right
+                ctx.lineTo(postionX + gridSize, postionY + gridSize);
+                Line.X = postionX + gridSize
+                Line.Y = postionY + gridSize
+            } else if (pickingThree == 2) {
+                //left up
+                ctx.lineTo(postionX - gridSize, postionY);
+                Line.X = postionX - gridSize
 
-        if (pickingTwo == 1) {
-            //right
-            ctx.lineTo(postionX + gridSize, postionY);
-             Line.X = postionX + gridSize
-            
-        } else {
-            //left
-            ctx.lineTo(postionX - gridSize, postionY);
-             Line.X = postionX - gridSize
-            
-        }
+            } else {
+                //left down
+                ctx.lineTo(postionX - gridSize, postionY + gridSize + gridSize);
+                Line.X = postionX - gridSize
+                Line.Y = postionY + gridSize + gridSize
 
-
-    }else{
-        console.log("x=5 y=1")
-        /*
-\__/--\__
-2 can go left or right 
-*/
-        ctx.moveTo(postionX, postionY + gridSize)
-        if (pickingTwo == 1) {
-            //left
-            ctx.lineTo(postionX - gridSize, postionY + gridSize);
-             Line.X = postionX - gridSize
-            Line.Y = postionY + gridSize
-        } else {
-            //right
-            ctx.lineTo(postionX + gridSize, postionY + gridSize);
-             Line.X = postionX + gridSize
-            Line.Y = postionY + gridSize
+            }
         }
     }
+
+
+    else if (gridX == 5) {
+        if (gridY == 0) {
+
+            /*
+        \__/--\__
+        5 can go rigth or left
+            */
+
+            if (pickingTwo == 1) {
+                //right
+                ctx.lineTo(postionX + gridSize, postionY);
+                Line.X = postionX + gridSize
+
+            } else {
+                //left
+                ctx.lineTo(postionX - gridSize, postionY);
+                Line.X = postionX - gridSize
+
+            }
+
+
+        } else {
+
+            /*
+    \__/--\__
+    2 can go left or right 
+    */
+            ctx.moveTo(postionX, postionY + gridSize)
+            if (pickingTwo == 1) {
+                //left
+                ctx.lineTo(postionX - gridSize, postionY + gridSize);
+                Line.X = postionX - gridSize
+                Line.Y = postionY + gridSize
+            } else {
+                //right
+                ctx.lineTo(postionX + gridSize, postionY + gridSize);
+                Line.X = postionX + gridSize
+                Line.Y = postionY + gridSize
+            }
+        }
 
 
     } else {
-        console.log("error")
+
     }
 
 
